49c43070aa3abd8ec9c5e073f57c1ff920fba042
LiloFr <lilo.frich@gmail.com>
2023-03-28 10:49:36 +0300

    Добавьте файлы проекта.

---
+++ WpfApp3/ViewModels/Base/MainWindowViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WpfApp3.ViewModels.Base
+{
+    internal class MainWindowViewModel : ViewModel
+    {
+        private string _Title;
+        public string Title
+        {
+            get => _Title;
+            set => Set(ref _Title, value);
+        }
+    }
+}
---
+++ WpfApp3/ViewModels/Base/ViewModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace WpfApp3.ViewModels.Base
+{
+    internal abstract class ViewModel : INotifyPropertyChanged, IDisposable
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged([CallerMemberName] string PropertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(PropertyName));
+        }
+        protected virtual bool Set<T>(ref T filed, T value, [CallerMemberName] string PeopsertName = null)
+        {
+            if (Equals(filed, value)) return false;
+            filed = value;
+            OnPropertyChanged(PeopsertName);
+            return true;
+        }
+        //~ViewModel()
+        //{
+        //    Dispose(false);
+        //}
+        public void Dispose()
+        {
+            Dispose(true);
+        }
+        private bool _Disposed;
+        protected virtual void Dispose(bool Disposing)
+        {
+            if (!Disposing || _Disposed) return;
+            _Disposed = true;
+        }
+
+    }
+}
